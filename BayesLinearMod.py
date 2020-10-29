625c1337a6a3ca857ba7f35a3f953726863462b4
caoyihan77shelly@hotmail.com <caoyihan77shelly@hotmail.com>
2020-10-29 23:44:12 +0100

    add an example
    An example of Bayesian linear regression with package pymc3

--- BayesLinearMod.py
+++ BayesLinearMod.py
@@ -1,123 +1,152 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 28 16:53:28 2020
+Created on Thu Oct 29 13:47:05 2020
 
 @author: yihanc
-
-This program introduce you the simplies Bayesian linear regression with Python.
-We first simulate a data frame contaning x and y, then use MCMC for posterior 
-prameter estimation. 
 """
 
-import numpy as np
+
+# Pandas and numpy for data manipulation
 import pandas as pd
-import seaborn as sns
+import numpy as np
+np.random.seed(42)
+
+#sklearn for data set split
+from sklearn.model_selection import train_test_split
+ 
+#matplotlib for plotting figures
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.size'] = 16
+matplotlib.rcParams['figure.figsize'] = (9, 9)
 
 
-def simulate_linear_data(N, beta_0, beta_1, eps_sigma_sq):
-    """
-    Simulate a random dataset using a noisy
-    linear process.
+# PyMC3 for Bayesian Inference
+import pymc3 as pm
 
-    N: Number of data points to simulate
-    beta_0: Intercept
-    beta_1: Slope of univariate predictor, X
-    """
-    # Create a pandas DataFrame with column 'x' containing
-    # N uniformly sampled values between 0.0 and 1.0
-    df = pd.DataFrame(
-        {"x": 
-            np.random.RandomState(42).choice(
-                map(
-                    lambda x: float(x)/100.0, 
-                    np.arange(100)
-                ), N, replace=False
-            )
-        }
-    )
+"""
+load data
+the data can be download from https://archive.ics.uci.edu/ml/datasets/student+performance#
+make sure that .csv file is kept in your working directory
+"""
+#%%
+df = pd.read_csv('student-mat.csv', sep = ";")
 
-    # Use a linear model (y ~ beta_0 + beta_1*x + epsilon) to 
-    # generate a column 'y' of responses based on 'x'
-    eps_mean = 0.0
-    df["y"] = beta_0 + beta_1*df["x"] + np.random.RandomState(42).normal(
-        eps_mean, eps_sigma_sq, N
-    )
+df = df[~df['G3'].isin([0, 1])]
 
-    return df
+df = df.rename(columns={'G3': 'Grade'})
 
+df = df.drop(columns=['G1', 'G2'])
 
-if __name__ == "_ main__":
+df.head()
+#%%
+
+"""
+check corerlation between all the continuous variables
+check numerical correaltion between all the continuous variables and "Grade"
+"""
+#%%
+df.corr()
+
+# Correlations of continuous variables
+df.corr()['Grade'].sort_values()
+#%%
+
+"""
+check corerlation between all the continuous variables
+check numerical correaltion between all the continuous variables and "Grade"
+"""
+#%%
+# Select only categorical variables
+category_df = df.select_dtypes('object')
+# One hot encode the variables
+dummy_df = pd.get_dummies(category_df)
+# Put the grade back in the dataframe
+dummy_df['Grade'] = df['Grade']
+dummy_df.head()
+# Correlations in one-hot encoded dataframe
+dummy_df.corr()['Grade'].sort_values()
+#%%
+
+"""
+select from all the variable four variables which have highest correlation
+with "Grade" and split the data into traning set and test set
+"""
+
+#%%
+# grade and returns training and testing datasets
+def format_data(df):
+    # Targets are final grade of student
+    labels = df['Grade']
     
-    beta_0 = 1.0
-    beta_1 = 2.0
+    # Drop the school from features
+    df = df.drop(columns=['school'])
     
-    N=100
-    eps_sigma_sq=0.5
+    # One-Hot Encoding of Categorical Variables
+    df = pd.get_dummies(df)
     
+    # Find correlations with the Grade
+    allcor = df.corr().abs()['Grade'].sort_values(ascending=False)
     
-df = simulate_linear_data(N, beta_0, beta_1, eps_sigma_sq)
+    # Maintain the top 6 most correlation features with Grade
+    most_correlated = allcor[:6]
+    
+    df = df.loc[:, most_correlated.index]
+    
+    df = df.drop(columns = 'schoolsup_no')
+    
+    # Split into training/testing sets with 25% split
+    X_train, X_test, y_train, y_test = train_test_split(df, labels, 
+                                                        test_size = 0.25,
+                                                        random_state=42)
+    #X_train and X_test contains all the variables including the response variable  
+    #y_train and y_test only contains the response variable
+    return X_train, X_test, y_train, y_test
 
-sns.lmplot(x="x", y="y", data=df, size=10)
-plt.xlim(0.0, 1.0)
+#%%
 
-           
-import PyMC3 as pm
-        
-def glm_mcmc_inference(df, iterations=5000):
-    """
-    Calculates the Markov Chain Monte Carlo trace of
-    a Generalised Linear Model Bayesian linear regression 
-    model on supplied data.
+"""
+Create linear regression model in PyMC3
+and sample posterior estimates
+"""
+#%%
 
-    df: DataFrame containing the data
-    iterations: Number of iterations to carry out MCMC for
-    """
-    # Use PyMC3 to construct a model context
-    basic_model = pm.Model()
-    with basic_model:
-        # Create the glm using the Patsy model syntax
-        # We use a Normal distribution for the likelihood
-        pm.glm.glm("y ~ x", df, family=pm.glm.families.Normal())
+# run customized function format_data first
+format_data(df)
 
-        # Use Maximum A Posteriori (MAP) optimisation as initial value for MCMC
-        start = pm.find_MAP()
+# Formula for Bayesian Linear Regression (follows R formula syntax
+formula = 'Grade ~ ' + ' + '.join(['%s' % variable for variable in X_train.columns[1:]])
+formula
 
-        # Use the No-U-Turn Sampler
-        step = pm.NUTS()
+with pm.Model() as normal_model:
+    
+    # The prior for the model parameters will be a normal distribution
+    family = pm.glm.families.Normal()
+    
+    # Creating the model requires a formula and data (and optionally a family)
+    pm.GLM.from_formula(formula, data = X_train, family = family)
+    
+    # Perform Markov Chain Monte Carlo sampling
+    normal_trace = pm.sample(draws=2000, chains = 2, tune = 500,cores=1)
 
-        # Calculate the trace
-        trace = pm.sample(
-            iterations, step, start, 
-            random_seed=42, progressbar=True
-        )
-
-    return trace
-
-...
-...
-
-if __name__ == "__main__":
-    ...
-    ...
-    trace = glm_mcmc_inference(df, iterations=5000)
-    pm.traceplot(trace[500:])
-    plt.show()   
+#%%
 
 
-# Plot a sample of posterior regression lines
-    sns.lmplot(x="x", y="y", data=df, size=10, fit_reg=False)
-    plt.xlim(0.0, 1.0)
-    plt.ylim(0.0, 4.0)
-    pm.glm.plot_posterior_predictive(trace, samples=100)
-    x = np.linspace(0, 1, N)
-    y = beta_0 + beta_1*x
-    plt.plot(x, y, label="True Regression Line", lw=3., c="green")
-    plt.legend(loc=0)
-    plt.show()    
+"""
+Some useful functions for the posterior inspection
+"""
+#%%
+pm.summary(normal_trace)
+plot_trace(normal_trace)
+pm.forestplot(normal_trace)
+pm.plot_posterior(normal_trace, figsize = (14, 14), text_size=20);
+
+#%%
 
 
 
-         
-        
+
+
+
+
